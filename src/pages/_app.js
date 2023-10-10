f0ee4564b7cc29f7dfaa9b5307aa99a2976b37b3
Yunus <Yunus0261@gmail.com>
2023-10-10 10:00:13 +0300

    redux path desing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
 import Layout from "../../components/layout/layout";
-import {configureStoreWith} from "@/redux/store";
+import { configureStoreWith } from "../redux/store";
 import { Provider } from "react-redux";
 
 const store = configureStoreWith();
 export default function App({ Component, pageProps }) {
   return (
     <div>
-        <Provider store={store}>
+      <Provider store={store}>
         <CssVarsProvider disableTransitionOnChange>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </CssVarsProvider>
-        </Provider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </CssVarsProvider>
+      </Provider>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,17 @@
-import {offerRequestSlice} from "@/redux/offer-request/offer-request.slice";
-import {singleContentSlice} from "@/redux/single-content/content.slice";
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import {languageSlice} from "@/redux/types/language/language.slice";
-import {contentTypeSlice} from "@/redux/types/content-type/content-type.slice";
-
+import { offerRequestSlice } from "./offer-request//offer-request.slice";
+import { singleContentSlice } from "./single-content/content.slice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { languageSlice } from "./types/language/language.slice";
+import { contentTypeSlice } from "./types/content-type/content-type.slice";
 
 const rootReducer = combineReducers({
-    offerRequest: offerRequestSlice.reducer,
-    singleContent: singleContentSlice.reducer,
-    language: languageSlice.reducer,
-    contentType: contentTypeSlice.reducer,
+  offerRequest: offerRequestSlice.reducer,
+  singleContent: singleContentSlice.reducer,
+  language: languageSlice.reducer,
+  contentType: contentTypeSlice.reducer,
 });
 
 export const configureStoreWith = () =>
-    configureStore({
-        reducer: rootReducer,
-    });
+  configureStore({
+    reducer: rootReducer,
+  });
