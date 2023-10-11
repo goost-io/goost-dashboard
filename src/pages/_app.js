455a6837b30425f91fd10a99b1df344205b39060
erkam <erkam.bozan@gmail.com>
2023-10-11 11:37:13 +0300

    fixing login sidebar issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,25 @@ import { CssVarsProvider } from "@mui/joy/styles";
 import Layout from "../../components/layout/layout";
 import { Provider } from "react-redux";
 import {configureStoreWith} from "@/redux/store";
+import {useRouter} from "next/router";
 
 const store = configureStoreWith();
 export default function App({ Component, pageProps }) {
-  return (
+
+    const router = useRouter();
+
+    // Define an array of routes where you don't want to use the Layout component
+    const routesWithoutLayout = ['/login']; // Add the routes where Layout is not needed
+
+    // Check if the current route is in the array of routes without Layout
+    const useLayout = !routesWithoutLayout.includes(router.pathname);
+
+    return (
     <div>
       <Provider store={store}>
         <CssVarsProvider disableTransitionOnChange>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+            {useLayout && <Layout><Component {...pageProps}/></Layout>}
+            {!useLayout && <Component {...pageProps}/>}
         </CssVarsProvider>
       </Provider>
     </div>
